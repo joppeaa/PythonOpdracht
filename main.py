a3e93a271d2a34d865a673073dfda30ef84ac8e3
Joppe Aarnoutse <joppeaa@gmail.com>
2022-12-06 17:57:21 +0100

    Added readme, extra option for substitution cipher, text input verification

--- main.py
+++ main.py
@@ -5,15 +5,31 @@ def encryptText():
     print("Encrypting option selected")
     newAlphabet = []
     
-    inputText = input('Please enter the the text to be encrypted: ')                            #Receiving the original text to be encrypted
-    print("Text to be encrypted: ", inputText)
+    while True:
+        try:
+            inputText = input('Please enter the the text to be encrypted: ')                            #Receiving the original text to be encrypted
+            #print("Text to be encrypted: ", inputText)
+            #print("Selected key: ", key)
+            for i in range(len(inputText)):                                                             #Getting the length of the input-text and looping over each character in this range
+                char = inputText[i].lower()
+                if char not in alphabet:
+                    raise TypeError("Only character from alphabet allowed")
+ 
+            break
+        except:
+            ("Please enter a text with valid character included in the normal alphabet range")
     
-    key = int(input('Please enter the encryption key: '))                                       #Receiving the encryption key/offset
-    print("Selected key: ", key)
-          
+    while True:
+        try:
+            key = int(input('Please enter the encryption key: '))                                   #Receiving the encryption key/offset
+            break
+        except:
+            print("Please enter a integer value")
+
+
     for i in range(len(inputText)):                                                             #Getting the length of the input-text and looping over each character in this range
-        char = inputText[i]
-        #print(alphabet.index(char))
+        char = inputText[i].lower()
+        print(alphabet.index(char))
 
         newCharLocation = int(alphabet.index(char) + key)                                       #Appying the key-offset to the characters
         if newCharLocation > len(alphabet):
@@ -21,8 +37,8 @@ def encryptText():
         elif newCharLocation < 0:
             newCharLocation = (len(alphabet) + newCharLocation)
         
-        #print(newCharLocation)
-        #print(alphabet[newCharLocation])
+        print(newCharLocation)
+        print(alphabet[newCharLocation])
         newAlphabet.insert(i, (alphabet[newCharLocation]))                                      #Filling a list with the new characters
         
     print(newAlphabet)
@@ -32,25 +48,30 @@ def encryptText():
     print("Moving back to the selection menu")
  
 def decryptText():
+
     print("Decrypting option selected")
     
 
 def findKey():
     print("Finding key option selected")
 
+def customCipher():
+    print("Custom substituion cipher option selected")
+
 
 def selectionHandler():                                                                         #Starting function to select the desired option
     chosenOption = 0
     print('\033[1m' + '\nWelcome to the caesar cipher solver by: Joppe Aarnoutse, Floris Wondergem, and Joël David')   
     print('\033[0m')
-    while chosenOption not in [1,2,3]:
+    while chosenOption not in [1,2,3,4]:
         
         print("Option 1: Encrypt readable text with key K")
         print("Option 2: Decrypt text with key K")
         print("Option 3: Find the key K")
+        print("Option 4: Encrypt with custom substitution cipher")
 
-        chosenOption = int(input("\nPlease enter the value of the desired option, 1-3: "))
-        if chosenOption not in [1,2,3]:
+        chosenOption = int(input("\nPlease enter the value of the desired option, 1-4: "))
+        if chosenOption not in [1,2,3,4]:
             print("\n***Input not valid!, please choose one of the availible options***")
     return chosenOption
 
@@ -60,10 +81,12 @@ while True:
     option = selectionHandler()
     if option == 1:
         encryptText()
-    elif option ==2:
+    elif option == 2:
         decryptText()
-    elif option ==3:
+    elif option == 3:
         findKey()
+    elif option == 4:
+        customCipher()
     else:
         print("Option selection failed, please try again.")
 
---
+++ readme.md
@@ -0,0 +1,9 @@
+Welcome to the caesar cipher solver by: Joppe Aarnoutse, Floris Wondergem, and Joël David
+
+This solver consists of 4 options:
+
+    Encrypting a entered text with a certain key
+    Decrypting a entered text with a certain key
+    Determining the key from a encrypted text(hacking the encryption)
+
+There is some basic input verification implemented. However, please pay attention to what input is requested in order to fully use this tool.
